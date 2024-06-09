5c6e0ff9ca9ea53f3c09776abeaa8ebec4143d5c
quratulainasiya <162177679+quratulainasiya@users.noreply.github.com>
2024-06-09 22:24:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict";
+//  Q3
+// let personName="Abdullah";
+// console.log(personName);
+// console.log(personName.toLowerCase());
+// console.log(personName.toUpperCase());
+// console.log(personName.toLowerCase());
+// console.log(personName.charAt(0).toUpperCase+personName.slice(1).toLowerCase());
+// Q4
+// let famPerson="Einstein";
+// let famQuote="A person who never made a mistake never tried anything new.";
+// console.log(`${famPerson} once said, "${famQuote}"`);
+// // Q5:
+// let famousPerson:string="Albert Einstein";
+// let message:string=`${famousPerson} once said, '${famQuote}"`;
+// console.log(message);
+// // Q6:
+// let namewithWhitespaces:string="\t\n Annee \t\n";
+// console.log("Name with white spaces:" ,namewithWhitespaces);
+// let strippedName:string=(namewithWhitespaces.trim());
+// console.log("Stripped Names:", strippedName);
+// Q7:
+// let additionResult:number=5+3;
+// console.log("Addition Result:", additionResult);
+// let subtractionResult:number=10-2;
+// console.log("Substraction Result:", subtractionResult)
+// let multiplicationResult:number=2*4;
+// console.log("Multiplication Result:" , multiplicationResult);
+// let divisionResult:number=16/2;
+// console.log("Division Result:", divisionResult);
+// Q8:
+// console.log(5+3);
+// console.log(10-2);
+// console.log(2*4);
+// console.log(16/2);
+// Q9:
+// let myfavNum:number=5;
+//  let messageResult:string=(`My favourite number is:", ${myfavNum}`);
+//  console.log(messageResult);
+//  Q10:
+// 
+// console.log(5+3);This program adds two numbers and prints.
+// console.log(10-2);This program subtracts two numbers and prints.
+// console.log(2*4);This program multiplies two numbers and prints.
+// console.log(16/2);This program divides two numbers and prints.
